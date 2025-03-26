a2e656e39e12ebfc150ce5a2e5b7d251bf10df25
Alex41099 <nevazhnoalikhan@gmail.com>
2025-03-26 19:32:27 +0500

    commit end

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = (env) => {
     return {
         mode: env.mode ?? 'development',
-        entry: path.resolve(__dirname, 'src', 'index.js'), // путь к точке входа в приложение
+        entry: path.resolve(__dirname, 'src', 'index.ts'), // путь к точке входа в приложение
         output: { // место где будет собираться сборка
             path: path.resolve(__dirname, 'build'),
             filename: '[name].[contenthash].js',
@@ -12,7 +12,19 @@ module.exports = (env) => {
         },
         plugins: [
             new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})
-        ]
+        ],
+        module: {
+            rules: [ // лоадеры
+                {
+                    test: /\.tsx?$/, // обрабатываем ts и tsx
+                    use: 'ts-loader', // название
+                    exclude: /node_modules/, // не смотрим папку node_modules
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],  // расширения которые необходимо обработать
+        },
     }
 
 }
