b6afc1d94169d967ba1c7d4c97528c9d2d6625ba
Alex41099 <nevazhnoalikhan@gmail.com>
2025-03-26 16:00:03 +0500

    commit end

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path')
 
-module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.js'), // путь к точке входа в приложение
-    output: { // место где будет собираться сборка
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].[contenthash].js',
-        clean: true // если будем менять название filename и снова собирать то старый файл удалится!
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.js'), // путь к точке входа в приложение
+        output: { // место где будет собираться сборка
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true // если будем менять название filename и снова собирать то старый файл удалится!
+        }
     }
+
 }
 
