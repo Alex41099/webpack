a32bcfafdd0ca83e212d84e76630c3f967bf0e57
Alex41099 <nevazhnoalikhan@gmail.com>
2025-03-30 12:43:53 +0500

    commit end

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,6 +5,33 @@ import {BuildOptions} from "../types/types";
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   const isDev = options.mode === 'development'
 
+  const assetsLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: 'asset/resource'
+  }
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'convertColors',
+                params: {
+                  currentColor: true
+                }
+              }
+            ]
+          }
+        }
+      }
+    ]
+  }
+
   const cssLoaderWithModules = {
     loader: 'css-loader',
     options: {
@@ -29,7 +56,9 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   }
 
   return [
+    assetsLoader,
     scssLoader,
-    tsLoader
+    tsLoader,
+    svgLoader
   ]
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import classes from './app.module.scss'
 import {Link, Outlet} from "react-router-dom";
+import cubsPhoto from '../assets/cubs.png'
+import unnamedPhoto from '../assets/unnamed.jpg'
+import IconSvg from '../assets/free-icon-font-chart-histogram-5528038.svg'
+
 
 export const App = () => {
   return (
     <div>
+      <div>
+        <img width={100} height={100} src={cubsPhoto} />
+        <img width={100} height={100} src={unnamedPhoto}/>
+        <IconSvg style={{color: 'green'}} />
+      </div>
       <Link to={'/about'}>About</Link>
       <Link to={'/shop'}>Shop</Link>
       <button className={classes.button}>+</button>
